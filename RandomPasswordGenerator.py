513e2ab9dab54006b47438a755eeff19b9be8ece
j334sun <92042616+j334sun@users.noreply.github.com>
2022-02-06 01:36:34 -0500

    Update RandomPasswordGenerator.py

--- RandomPasswordGenerator.py
+++ RandomPasswordGenerator.py
@@ -2,14 +2,13 @@ import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-
-print("Welcome to the PyPassword Generator!")
+print("\n\n\n\nWelcome to the PyPassword Generator!")
 
 letters_count = int(input("How many letters would you like in your password?\n")) 
 
-symbols_count = int(input(f"How many symbols would you like?\n"))
+symbols_count = int(input("How many symbols would you like?\n"))
 
-numbers_count = int(input(f"How many numbers would you like?\n"))
+numbers_count = int(input("How many numbers would you like?\n"))
 
 str_l = ""
 str_s = ""
@@ -34,8 +33,6 @@ random.shuffle(ran_pass)
 
 passy = (''.join(ran_pass))
 
-print(f"Your password is {passy}.")
-
-
+print("Your password is {}.\n\n\n".format(passy))
 
 
